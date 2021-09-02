7371b85a6521274ec9673361d0e8e18e822faf18
luobozz <437337961@qq.com>
2021-09-02 15:39:03 +0800

    add push for daily!

--- daily/daily.cmd
+++ daily/daily.cmd
@@ -6,7 +6,8 @@ set MAX_INPUT_RETRY=3
 set TODAY=%DATE:~0,4%-%DATE:~5,2%-%DATE:~8,2%
 
 echo "1. add a daily <.md>"
-echo "2. say hi!"
+echo "2. push daily to github"
+echo "3. say hi!"
 echo "please choose [1-2]:"
 
 set /p input_source=
@@ -26,10 +27,15 @@ if "%input_source%"=="1" (
     IF NOT EXIST %DAILY_HOME% (
         MD %DAILY_HOME%
     )
+    git -C %DAILY_HOME%\ pull
     echo # %TODAY%>%DAILY_HOME%\%TODAY%.md
     echo %DAILY_HOME%\%TODAY%.md create success..
-    code %DAILY_HOME%\%TODAY%.md
+    code -n %DAILY_HOME%
 ) else if "%input_source%"=="2" (
+    git -C %DAILY_HOME%\ add .
+    git -C %DAILY_HOME%\ commit -am "daily:)"
+    git -C %DAILY_HOME%\ push
+) else if "%input_source%"=="3" (
     echo hi,three!
 ) else if "%input_source%" NEQ "" (
     echo command [ %input_source% ] is not supported..
